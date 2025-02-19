f4b9dad90db0d108bf2b6cbefd18fd19ee48caf3
Remus Schnell <schnell_remus@yahoo.com>
2025-02-19 21:53:42 +0100

    use 4 loops, but less memory. Same running time

--- sol08.py
+++ sol08.py
@@ -1,7 +1,6 @@
 
 import time
 
-import utils
 
 def sol():
     start = time.perf_counter()
@@ -18,19 +17,12 @@ def sol():
 
     visibility_score = [[1] * m for i in range(n)]
 
-    max_cols = [init_max]*m
-    cols_visibility = [[0] * m for i in range(n)]
-
     for i in range(n):
         max_row = init_max
         row_visibility = [0] * m
         for j in range(m):
             if forest[i][j] > max_row:
                 max_row = forest[i][j]
-                is_visible[i][j] = True
-            
-            if forest[i][j] > max_cols[j]:
-                max_cols[j] = forest[i][j]
                 is_visible[i][j] = True
 
             if j > 0:
@@ -40,33 +32,37 @@ def sol():
                         k -= row_visibility[k]
                     else:
                         break
-                
+
                 row_visibility[j] = j - k
+
+            visibility_score[i][j] *= row_visibility[j]
+
+    for j in range(m):
+        max_col = init_max
+        col_visibility = [0] * n
+        for i in range(n):
+            if forest[i][j] > max_col:
+                max_col = forest[i][j]
+                is_visible[i][j] = True
 
             if i > 0:
                 l = i - 1
                 while l>0:
                     if (forest[i][j] > forest[l][j]):
-                        l -= cols_visibility[l][j]
+                        l -= col_visibility[l]
                     else:
                         break
                 
-                cols_visibility[i][j] = i - l
+                col_visibility[i] = i - l
 
-            visibility_score[i][j] *= row_visibility[j] * cols_visibility[i][j]
+            visibility_score[i][j] *= col_visibility[i]
 
-    max_cols = [init_max]*m
-    cols_visibility = [[0] * m for i in range(n)]
     for i in range(n-1, -1, -1):
         max_row = init_max
         row_visibility = [0] * m
         for j in range(m-1, -1, -1):
             if forest[i][j] > max_row:
                 max_row = forest[i][j]
-                is_visible[i][j] = True
-
-            if forest[i][j] > max_cols[j]:
-                max_cols[j] = forest[i][j]
                 is_visible[i][j] = True
 
             if j < m-1:
@@ -79,20 +75,29 @@ def sol():
                 
                 row_visibility[j] = k - j
 
+            visibility_score[i][j] *= row_visibility[j]
+
+    for j in range(m-1, -1, -1):
+        max_col = init_max
+        col_visibility = [0] * n
+        for i in range(n-1, -1, -1):
+            if forest[i][j] > max_col:
+                max_col = forest[i][j]
+                is_visible[i][j] = True
+
             if i < n-1:
                 l = i + 1
                 while l < n-1:
                     if (forest[i][j] > forest[l][j]):
-                        l += cols_visibility[l][j]
+                        l += col_visibility[l]
                     else:
                         break
                 
-                cols_visibility[i][j] = l - i
+                col_visibility[i] = l - i
 
-            visibility_score[i][j] *= row_visibility[j] * cols_visibility[i][j]
+            visibility_score[i][j] *= col_visibility[i]
 
     score1 = sum([sum(x) for x in is_visible])
-
     score2 = max([max(v) for v in visibility_score])
 
     end = time.perf_counter()
@@ -104,6 +109,7 @@ def sol():
     print("First star: ", score1)
     print("Second star: ", score2)
     return
+
 
 if __name__ == "__main__":
     sol()
