2edf4f76389072eb37a4668559defacc9251fe85
Remus Schnell <schnell_remus@yahoo.com>
2025-02-27 19:12:56 +0100

    Day17: little optimization

--- sol17.py
+++ sol17.py
@@ -16,7 +16,7 @@ def sol():
     wind_directions = file.readline()
     file.close()
 
-    def findHeight(nbStones, useHistory):
+    def findHeight(nbStones1, nbStones2):
         stones = cycle(enumerate(kSTONES))
         winds = cycle(enumerate(wind_directions))
 
@@ -27,6 +27,7 @@ def sol():
 
         i = 0
         isCycleFound = False
+        nbStones = max(nbStones1, nbStones2)
         while i < nbStones:
             height = maxHeightNow()
             stone_idx, currStone = next(stones)
@@ -51,7 +52,7 @@ def sol():
                     hall |= curr_stone
                     break
 
-            if useHistory and not isCycleFound:
+            if not isCycleFound:
                 current_heights = [max([rock.imag for rock in hall if rock.real == x]) for x in range(0,7)]
                 min_current_height = min(current_heights)
                 height_profile = tuple([h - min_current_height for h in current_heights])
@@ -60,18 +61,44 @@ def sol():
                     oldi, max_height_than = history[stone_idx, wind_idx, height_profile]
                     delta_i = i - oldi
                     ncycles = (nbStones - i) // delta_i
-                    i += delta_i * ncycles
                     delta_height = maxHeightNow() - max_height_than
+
+#--------------------------------------------------------------------------------------------------------------------------------------------
+                    # INSERT OPTIMIZATION - NO CODE CLEANING:
+                    # just look in history for the height: solve both inputs:
+                    ncycles_1 = (nbStones1 - i) // delta_i
+                    ncycles_2 = (nbStones2 - i) // delta_i
+                    i1 = i + delta_i * ncycles_1
+                    i2 = i + delta_i * ncycles_2
+                    nb_remaining_rocks1 = nbStones1 - i1 - 1 # -1 becuase was incremetented just at the end, below
+                    nb_remaining_rocks2 = nbStones2 - i2 - 1 # -1 becuase was incremetented just at the end, below
+                    mm1 = None
+                    mm2 = None
+                    # Find nb_remaining_rocks in history:
+                    for key, item in history.items():
+                        if item[0] == nb_remaining_rocks1 + oldi:
+                            dd_height = item[1] - max_height_than
+                            mm1 = int(maxHeightNow() + dd_height + delta_height*ncycles_1)
+                        if item[0] == nb_remaining_rocks2 + oldi:
+                            dd_height = item[1] - max_height_than
+                            mm2 = int(maxHeightNow() + dd_height + delta_height*ncycles_2)
+                        if mm1 and mm2:
+                            return mm1, mm2
+                    #END OLTIMIZATION INSERTION - NO CODE CLEANING:
+#--------------------------------------------------------------------------------------------------------------------------------------------
+                    i += delta_i * ncycles
                     hall = set([x + delta_height*ncycles * 1j + h*1j for x,h in enumerate(current_heights)])
                     isCycleFound = True
+
                 else:
                     history[stone_idx, wind_idx, height_profile] = i, maxHeightNow()
 
             i += 1
-        return int(max(n.imag for n in hall))
 
-    score1 = findHeight(2022, False)
-    score2 = findHeight(1000000000000, True)
+        mm = maxHeightNow()
+        return mm, mm
+
+    score1, score2 = findHeight(2022, 1000000000000)
 
     end = time.perf_counter()
 
