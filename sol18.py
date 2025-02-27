aa013c8e635f828832cf3855a550a6f29becee5c
Remus Schnell <schnell_remus@yahoo.com>
2025-02-27 22:04:53 +0100

    Day18 - second star

--- sol18.py
+++ sol18.py
@@ -1,25 +1,57 @@
 import time
-
+import sys
 import utils
 
 def neighbours(xyz):
     x , y, z = xyz
-    return [(x,y,z-1),(x,y,z+1),(x-1,y,z),(x+1,y,z),(x,y-1,z),(x,y+1,z)]
+    return {(x,y,z-1),(x,y,z+1),(x-1,y,z),(x+1,y,z),(x,y-1,z),(x,y+1,z)}
 
+def countFaces(shape):
+    nbFaces = len(shape) * 6
+    for cube in shape:
+        nbFaces -= len(shape & neighbours(cube))
+    return nbFaces
 
 def sol():
     start = time.perf_counter()
 
-    nums = set()
+    droplet = set()
     with open('inputs/input18.txt', 'r') as file:
         for line in file:
-            nums.add(tuple(utils.extractAllNumbers(line)))
+            droplet.add(tuple(utils.extractAllNumbers(line)))
 
-    score1 = len(nums) * 6
-    for cube in nums:
-        score1 -= len([nei for nei in neighbours(cube) if nei in nums])
+    # First star
+    score1 = countFaces(droplet)
 
-    score2 = 0
+    # Second star:
+    x_min = min([cube[0] for cube in droplet]) - 1
+    x_max = max([cube[0] for cube in droplet]) + 1
+    y_min = min([cube[1] for cube in droplet]) - 1
+    y_max = max([cube[1] for cube in droplet]) + 1
+    z_min = min([cube[2] for cube in droplet]) - 1
+    z_max = max([cube[2] for cube in droplet]) + 1
+
+    empty_space = set()
+    for x in range(x_min, x_max + 1):
+        for y in range(y_min, y_max + 1):
+            for z in range(z_min, z_max + 1):
+                empty_space.add((x,y,z))
+    empty_space -= droplet
+
+    sys.setrecursionlimit(15000)
+
+    def findInnerSpace(cube, space: set):
+        if cube not in space:
+            return
+
+        space.remove(cube)  
+        nei = neighbours(cube) & space
+
+        for other in nei:
+            findInnerSpace(other, space)
+
+    findInnerSpace((0,0,0), empty_space)
+    score2 = score1 - countFaces(empty_space)
 
     end = time.perf_counter()
 
