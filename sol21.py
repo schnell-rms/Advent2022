54f8e6c2f7618de35eeaf0f074226005cec73335
Remus Schnell <schnell_remus@yahoo.com>
2025-02-28 18:16:45 +0100

    Day21 second star

--- sol21.py
+++ sol21.py
@@ -11,6 +11,32 @@ class Monkey:
             self.operand_2 = instruction[-4:]
             self.operator = instruction[5]
 
+        self.has_human_scream = False # aka it includes human input too
+
+def revertOperator_Op1(operator, result, op2):
+    match operator:
+        case '+':
+            return result - op2
+        case '-':
+            return result + op2
+        case '/':
+            return result * op2
+        case '*':
+            return result // op2
+    return None
+
+def revertOperator_Op2(operator, result, op1):
+    match operator:
+        case '+':
+            return result - op1
+        case '-':
+            return op1 - result
+        case '/':
+            return op1 // result
+        case '*':
+            return result // op1
+    return None
+
 def sol():
 
     start = time.perf_counter()
@@ -22,22 +48,48 @@ def sol():
             name, instruction = line.split(":")
             monkeys[name] = Monkey(instruction)
 
+    monkeys["humn"].has_human_scream = True
+
     # First star
     def scream(name):
         monkey = monkeys[name]
         if monkey.scream != None:
-            return monkey.scream
+            return monkey.scream, monkey.has_human_scream
         
-        m1 = scream(monkey.operand_1)
-        m2 = scream(monkey.operand_2)
+        m1, is_human_1 = scream(monkey.operand_1)
+        m2, is_human_2 = scream(monkey.operand_2)
 
         monkey.scream = int(eval(str(m1) + monkey.operator + str(m2)))
-        return monkey.scream
+        monkey.has_human_scream = is_human_1 or is_human_2
+        return monkey.scream, monkey.has_human_scream
 
-    score1 = scream("root")
+    score1, _ = scream("root")
 
     # Second star:
-    score2 = 0
+    def setScream(monkey, value):
+        # Set value to this name
+        if not hasattr(monkey, "operator"):
+            # The human scream:
+            monkey.scream = value
+            return
+
+        # Has operator:
+        if monkeys[monkey.operand_1].has_human_scream:
+            fixed_value = monkeys[monkey.operand_2].scream
+            needed_value = revertOperator_Op1(monkey.operator, value, fixed_value)
+            setScream(monkeys[monkey.operand_1], needed_value)
+        else:
+            fixed_value = monkeys[monkey.operand_1].scream
+            needed_value = revertOperator_Op2(monkey.operator, value, fixed_value)
+            setScream(monkeys[monkey.operand_2], needed_value)
+
+    root_monkey = monkeys["root"]
+
+    needed_value = monkeys[root_monkey.operand_1].scream if not monkeys[root_monkey.operand_1].has_human_scream else monkeys[root_monkey.operand_2].scream
+    human_monkey = monkeys[root_monkey.operand_1] if monkeys[root_monkey.operand_1].has_human_scream else monkeys[root_monkey.operand_2]
+
+    setScream(human_monkey, needed_value)
+    score2 = monkeys["humn"].scream
 
     end = time.perf_counter()
 
