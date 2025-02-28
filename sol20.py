77285de661344a7fdd13626f73ad201f6d0faab1
Remus Schnell <schnell_remus@yahoo.com>
2025-02-28 14:02:50 +0100

    Day20 second star

--- sol20.py
+++ sol20.py
@@ -17,26 +17,28 @@ def sol():
             id += 1
 
     cp = list(numbers)
-    for num in cp:
-        pos = [idx for idx, val in enumerate(numbers) if num.orig_idx == val.orig_idx][0]
-        numbers.rotate(-pos)
-        numbers.popleft()
-        numbers.rotate(-num.value)
-        numbers.appendleft(num)
-
-
-    print(num)
-
-    pos0 = [idx for idx, num in enumerate(numbers) if num.value == 0][0]
-    numbers.rotate(-pos0)
-
     n = len(numbers)
 
+    def decrypt(numbers, factor, repeat):
+        for _ in range(repeat):
+            for num in cp:
+                pos = [idx for idx, val in enumerate(numbers) if num.orig_idx == val.orig_idx][0]
+                numbers.rotate(-pos)
+                numbers.popleft()
+                numbers.rotate( - ((num.value * factor) % (n-1)))
+                numbers.appendleft(num)
+
+        pos0 = [idx for idx, num in enumerate(numbers) if num.value == 0][0]
+        numbers.rotate(-pos0)
+
+        return factor * (numbers[1000 % n][1] + numbers[2000 % n][1] + numbers[3000 % n][1])
+
     # First star
-    score1 = numbers[1000 % n][1] + numbers[2000 % n][1] + numbers[3000 % n][1]
+    numbers2 = numbers.copy()
+    score1 = decrypt(numbers, 1, 1)
 
     # Second star:
-    score2 = 0
+    score2 = decrypt(numbers2, 811589153, 10)
 
     end = time.perf_counter()
 
